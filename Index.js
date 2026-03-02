fdc87ca3417b699aa05d58dc6eb7a0d336494d05
Saif- <omanagerad@DESKTOP-PHRCPHH>
2026-03-02 13:14:19 +0100

    Added , so we can save the file or mapp we create

--- Index.js
+++ Index.js
@@ -7,7 +7,46 @@ function showScreen(id){
 }
 
 function saveQuestion(){
-    alert("Frågan sparades!");
+
+    const title = document.getElementById("quizTitle").value;
+    const answers = document.querySelectorAll("#createQuestion .answerInput");
+
+    if(title === ""){
+        alert("Skriv en titel!");
+        return;
+    }
+
+    let answerArray = [];
+
+    answers.forEach(input => {
+        if(input.value !== "" && input.id !== "quizTitle"){
+            answerArray.push(input.value);
+        }
+    });
+
+    if(answerArray.length === 0){
+        alert("Skriv minst ett svar!");
+        return;
+    }
+
+    // Skapa quiz-objekt
+    const quiz = {
+        title: title,
+        answers: answerArray
+    };
+
+    // Hämta gamla quiz
+    let savedQuizzes = JSON.parse(localStorage.getItem("quizzes")) || [];
+
+    // Lägg till nytt quiz
+    savedQuizzes.push(quiz);
+
+    // Spara igen
+    localStorage.setItem("quizzes", JSON.stringify(savedQuizzes));
+
+    alert("Quiz sparat!");
+
+    goBack();
 }
 function goBack(){
 
@@ -18,3 +57,10 @@ function goBack(){
     // Gå tillbaka till titleScreen
     showScreen('titleScreen');
 }
+function loadQuizzes(){
+
+    let savedQuizzes = JSON.parse(localStorage.getItem("quizzes")) || [];
+
+    console.log("Sparade quiz:", savedQuizzes);
+}
+loadQuizzes();
